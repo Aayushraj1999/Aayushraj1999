2b77620a2ec0f69f194912e21456c51e2bb9797f
Aayushraj1999 <ayushraj109@gmail.com>
2023-10-03 15:28:03 +0530

    checking url hits and there response

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+
+// const http = require('http');
+
+// const server =  http.createServer((req, res) => {
+//   console.log(req);
+    
+// }); 
+
+// server.listen(4000,()=>{
+//   console.log("app is running at port 4000")
+// });
+
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
- console.log('Aayush Raj')
+  const url= req.url;
+  if (url == '/home') {
+    res.setHeader('content-Type','text/html' );
+    res.write('<html>')
+    res.write('<body>Welcome home </body>')
+    res.write('</html>')
+  } else if (url == '/about') {
+    res.setHeader('content-Type', 'text/html' );
+    res.write('<html>')
+    res.write('<body>Welcome to about us page </body>')
+    res.write('</html>')
+  } else if (url == '/node') {
+    res.setHeader('content-Type', 'text/html' );
+    res.write('<html>')
+    res.write('<body>Welcome to my node Js project </body>')
+    res.write('</html>')
+  } 
 });
 
-server.listen(4000);
+server.listen(3000);
