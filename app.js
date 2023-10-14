da609d768d16ae6560fe3d0b03b06bb87e678b34
Aayushraj1999 <ayushraj109@gmail.com>
2023-10-14 16:32:48 +0530

    Create the /add-product route with using bodyparser

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
     next();
 });
 
-app.use((req,res,next)=> {
+
+app.use( '/add-product', (req, res, next)=> {
     console.log('In Another middleware!');
-    res.send('<h1> hello from express </h2>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post( '/product' , (req,res,next) => {
+   console.log(req.body);
+   res.redirect('/');
+});
+
+app.use( '/', (req, res, next)=> {
+    console.log('In Another middleware!');
+    res.send('<h1> Hello from express </h1>');
 });
 
 
